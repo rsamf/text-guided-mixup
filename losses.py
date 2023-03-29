280a76b126154936e9eb2b50df6b246027a41275
sammyfranklin <sammyfranklin@yahoo.com>
2023-03-29 11:25:05 -0700

    starting on LDAM and focal losses

--- losses.py
+++ losses.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.modules.loss import _Loss
 from utils import DEVICE
+import numpy as np
 
 class BalancedCE(_Loss):
     """
@@ -56,3 +57,39 @@ class AFS(_Loss):
                 sum += -torch.log(-ang * ind + 1)
                 sum_still_in_margin += ind
         return sum / sum_still_in_margin if sum_still_in_margin > 0 else 0
+
+class FocalLoss(nn.Module):
+    def __init__(self, weight=None, gamma=0.):
+        super(FocalLoss, self).__init__()
+        assert gamma >= 0
+        self.gamma = gamma
+        self.weight = weight
+
+    def forward(self, input, target):
+        cross_entropy = F.cross_entropy(input, target, reduction='none', weight=self.weight)
+        p = torch.exp(-cross_entropy)
+        loss = (1 - p) ** self.gamma * cross_entropy
+        return loss.mean()
+    
+class LDAMLoss(nn.Module):
+    def __init__(self, cls_num_list, max_m=0.5, weight=None, s=30):
+        super(LDAMLoss, self).__init__()
+        m_list = 1.0 / np.sqrt(np.sqrt(cls_num_list))
+        m_list = m_list * (max_m / np.max(m_list))
+        m_list = torch.cuda.FloatTensor(m_list)
+        self.m_list = m_list
+        assert s > 0
+        self.s = s
+        self.weight = weight
+
+    def forward(self, x, target):
+        index = torch.zeros_like(x, dtype=torch.uint8)
+        index.scatter_(1, target.data.view(-1, 1), 1)
+        
+        index_float = index.type(torch.cuda.FloatTensor)
+        batch_m = torch.matmul(self.m_list[None, :], index_float.transpose(0,1))
+        batch_m = batch_m.view((-1, 1))
+        x_m = x - batch_m
+    
+        output = torch.where(index, x_m, x)
+        return F.cross_entropy(self.s*output, target, weight=self.weight)
